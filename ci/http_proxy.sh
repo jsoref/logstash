b4e93475a5fb5d8d5808853703e0ebc08c785bdd
Pier-Hugues Pellerin <phpellerin@gmail.com>
2017-03-03 10:23:35 -0800

    super proxy

--- ci/http_proxy.sh
+++ ci/http_proxy.sh
@@ -4,7 +4,7 @@ sudo apt-get install -y squid3 net-tools ufw
 echo y | sudo ufw enable
 sudo ufw deny out https
 sudo ufw deny out http
-curl -I "https://github.com" --proxy https://127.0.0.1:3128
 wget -e use_proxy=yes -e http_proxy=127.0.0.1:3128 -e https_proxy=127.0.0.1:3128 https://github.com
+curl -I "https://github.com" --proxy "https://localhost:3128"
 sudo cat /var/log/squid3/access.log
 sudo ufw disable
