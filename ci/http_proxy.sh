cdcf3c8e1c1f903eeae883ff4bb3be974974104c
Pier-Hugues Pellerin <phpellerin@gmail.com>
2017-03-03 14:10:51 -0800

    testing the right scripts

--- ci/http_proxy.sh
+++ ci/http_proxy.sh
@@ -1,10 +1,3 @@
 #!/usr/bin/env bash
 set -e
-sudo apt-get install -y squid3 net-tools ufw
-echo y | sudo ufw enable
-sudo ufw deny out https
-sudo ufw deny out http
-wget -e use_proxy=yes -e http_proxy=127.0.0.1:3128 -e https_proxy=127.0.0.1:3128 https://github.com
-curl -I "https://github.com" --proxy "http://localhost:3128"
-sudo cat /var/log/squid3/access.log
-sudo ufw disable
+bundle exec rspec spec/cli/http_proxy_install_spec.rb
