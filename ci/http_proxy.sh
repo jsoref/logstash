58746749665d0aaffa4f0ee1cbebf22f53b249a6
Pier-Hugues Pellerin <phpellerin@gmail.com>
2017-03-03 13:31:52 -0800

    testing curl

--- ci/http_proxy.sh
+++ ci/http_proxy.sh
@@ -5,6 +5,6 @@ echo y | sudo ufw enable
 sudo ufw deny out https
 sudo ufw deny out http
 wget -e use_proxy=yes -e http_proxy=127.0.0.1:3128 -e https_proxy=127.0.0.1:3128 https://github.com
-curl -I "https://github.com" --proxy "https://localhost:3128"
+curl -I "https://github.com" --proxy "http://localhost:3128"
 sudo cat /var/log/squid3/access.log
 sudo ufw disable
